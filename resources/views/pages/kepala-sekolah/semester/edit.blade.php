eb5d8bdc62b9ecbf4241840457dc1eda1c206a5b
Nusalendra <nusalendraa@gmail.com>
2024-10-04 08:35:01 +0700

    fix:memperbarui form edit semester

--- resources/views/pages/kepala-sekolah/semester/edit.blade.php
+++ resources/views/pages/kepala-sekolah/semester/edit.blade.php
@@ -1,43 +1,58 @@
 @extends('layouts.user_type.kepala-sekolah.form')
 
 @section('content')
-<div class="container-fluid py-4 mt-10">
-    <div class="card">
-        <div class="card-header pb-0 px-3">
-            <h6 class="mb-0">Edit Data Semester</h6>
-        </div>
-        <div class="card-body pt-4 p-3">
-            <form action="/semester/{{ $data->id }}" method="POST" role="form text-left">
-                @method('PUT')
-                @csrf
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <label for="nama_kelas" class="form-control-label">Kelas</label>
-                            <input type="hidden" name="kelas_id" value="{{ $waliKelas->kelas_id }}">
-                            <input class="form-control" type="text" value="{{ $waliKelas->kelas->nama }}" placeholder="Masukkan Semester" name="nama_kelas" disabled>
-                        </div>
-                        <div class="form-group">
-                            <label for="nama" class="form-control-label">Semester</label>
-                            <input class="form-control" type="text" value="{{ $data->semester->nama }}" placeholder="Masukkan Semester" name="nama">
-                        </div>
-                        <div class="form-group">
-                            <label for="status" class="form-control-label">Status Semester</label>
-                            <select name="status" id="status" class="form-select">
-                                <option value="0" disabled>Pilih Status Semester</option>
-                                <option value="Dibuka" {{ $data->status === 'Dibuka' ? 'selected' : '' }}>Dibuka</option>
-                                <option value="Ditutup" {{ $data->status === 'Ditutup' ? 'selected' : '' }}>Ditutup</option>
-                            </select>
+    <div class="container-fluid py-4 mt-10">
+        <div class="card">
+            <div class="card-header pb-0 px-3">
+                <h6 class="mb-0">Edit Data Semester</h6>
+            </div>
+            <div class="card-body pt-4 p-3">
+                <form action="/semester/{{ $data->id }}" method="POST" role="form text-left">
+                    @method('PUT')
+                    @csrf
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="tahun_ajaran" class="form-control-label">Tahun Ajaran</label>
+                                <input class="form-control" type="text" value="{{ $data->semester->tahun_ajaran }}"
+                                    placeholder="Masukkan Tahun Ajaran" name="tahun_ajaran">
+                            </div>
+                            <div class="form-group">
+                                <label for="tanggal_mulai" class="form-control-label">Tanggal Mulai</label>
+                                <input class="form-control" type="date" value="{{ $data->semester->tanggal_mulai }}"
+                                    placeholder="Masukkan Tanggal Mulai" name="tanggal_mulai">
+                            </div>
+                            <div class="form-group">
+                                <label for="tanggal_akhir" class="form-control-label">Tanggal Akhir</label>
+                                <input class="form-control" type="date" value="{{ $data->semester->tanggal_akhir }}"
+                                    placeholder="Masukkan Tanggal Akhir" name="tanggal_akhir">
+                            </div>
+                            <div class="form-group">
+                                <label for="nama_kelas" class="form-control-label">Kelas</label>
+                                <input type="hidden" name="kelas_id" value="{{ $data->kelas->id }}">
+                                <input class="form-control" type="text" value="{{ $data->kelas->nama }}"
+                                    placeholder="Masukkan Semester" name="nama_kelas" disabled>
+                            </div>
+                            <div class="form-group">
+                                <label for="nama" class="form-control-label">Semester <span
+                                        class="text-danger">*</span></label>
+                                <select name="nama" id="" class="form-select">
+                                    <option value="0" selected disabled>Pilih Semester</option>
+                                    <option value="Gasal" {{ $data->semester->nama === 'Gasal' ? 'selected' : '' }}>Gasal
+                                    </option>
+                                    <option value="Genap" {{ $data->semester->nama === 'Genap' ? 'selected' : '' }}>Genap
+                                    </option>
+                                </select>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="d-flex justify-content-end">
-                    <a href="/semester" class="btn bg-gradient-danger btn-md mt-4 mb-4 me-2">Kembali</a>
-                    <button type="submit" class="btn bg-gradient-info btn-md mt-4 mb-4">{{ 'Simpan' }}</button>
-                </div>
-            </form>
+                    <div class="d-flex justify-content-end">
+                        <a href="/semester" class="btn bg-gradient-danger btn-md mt-4 mb-4 me-2">Kembali</a>
+                        <button type="submit" class="btn bg-gradient-info btn-md mt-4 mb-4">{{ 'Simpan' }}</button>
+                    </div>
+                </form>
 
+            </div>
         </div>
     </div>
-</div>
 @endsection
