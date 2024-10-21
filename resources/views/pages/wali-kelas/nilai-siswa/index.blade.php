887ccbaa02e23937c5fad6e3c0551de5fa98b8d7
Nusalendra <nusalendraa@gmail.com>
2024-10-21 19:37:18 +0700

    fix:menu nilai siswa

--- resources/views/pages/wali-kelas/nilai-siswa/index.blade.php
+++ resources/views/pages/wali-kelas/nilai-siswa/index.blade.php
@@ -4,7 +4,7 @@
     <div>
         <div class="text-end">
             <a href="/nilai-siswa/create">
-                <button type="button" class="btn bg-gradient-info">Input Nilai</button>
+                <button type="button" class="btn bg-gradient-info">Input Nilai Siswa</button>
             </a>
         </div>
         <div class="py-1">
@@ -15,13 +15,13 @@
                             <thead>
                                 <tr>
                                     <th class="text-uppercase text-xs font-weight-bolder text-start">No</th>
-                                    <th class="text-uppercase text-xs font-weight-bolder">Semester</th>
                                     <th class="text-uppercase text-xs font-weight-bolder">Mata Pelajaran</th>
+                                    <th class="text-uppercase text-xs font-weight-bolder">Jenis Tugas</th>
                                     <th class="text-uppercase text-xs font-weight-bolder">Nama Tugas</th>
                                     <th class="text-uppercase text-xs font-weight-bolder">Aksi</th>
                                 </tr>
                             </thead>
-                            {{-- <tbody>
+                            <tbody>
                                 @foreach ($data as $index => $item)
                                     <tr>
                                         <td>
@@ -34,33 +34,33 @@
                                         <td>
                                             <div class="d-flex px-2 py-1">
                                                 <div class="d-flex flex-column justify-content-center">
-                                                    <h6 class="mb-0 text-sm">{{ $item->kelasSemester->semester->nama }}</h6>
+                                                    <h6 class="mb-0 text-sm">{{ $item->mataPelajaran->nama }}</h6>
                                                 </div>
                                             </div>
                                         </td>
                                         <td>
                                             <div class="d-flex px-2 py-1">
                                                 <div class="d-flex flex-column justify-content-center">
-                                                    <h6 class="mb-0 text-sm">{{ $item->kelasSemester->kelas->nama }}</h6>
+                                                    <h6 class="mb-0 text-sm">{{ $item->jenis_nilai }}</h6>
                                                 </div>
                                             </div>
                                         </td>
                                         <td>
                                             <div class="d-flex px-2 py-1">
                                                 <div class="d-flex flex-column justify-content-center">
-                                                    <h6 class="mb-0 text-sm">{{ $item->nama }}</h6>
+                                                    <h6 class="mb-0 text-sm">{{ $item->nama_tugas }}</h6>
                                                 </div>
                                             </div>
                                         </td>
                                         <td>
                                             <div class="d-flex px-2 py-1">
                                                 <div class="d-flex flex-column justify-content-center">
-                                                    <a href="/mata-pelajaran-guru/{{ $item->id }}">
+                                                    <a href="/nilai-siswa/{{ $item->id }}">
                                                         <button type="button" class="btn bg-gradient-info">
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                                                 <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                                                             </svg>
-                                                            Input Nilai Siswa
+                                                            Cek Nilai Siswa
                                                         </button>
                                                     </a>
                                                 </div>
@@ -68,7 +68,7 @@
                                         </td>
                                     </tr>
                                 @endforeach
-                            </tbody> --}}
+                            </tbody>
                         </table>
                         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
                         <script src="//cdn.datatables.net/2.0.3/js/dataTables.min.js"></script>
